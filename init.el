21678ceef49a2aade461d05289802a1d5aa130b6
Ioannis Eleftheriou <me@yath.xyz>
2024-01-18 00:29:42 +0000

    Add pass and editorconfig

--- init.el
+++ init.el
@@ -80,7 +80,7 @@
        (debugger +lsp)         ; FIXME stepping through code, to help you add bugs
        direnv
        docker
-       ;;editorconfig      ; let someone else argue about tabs vs spaces
+       editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
@@ -88,7 +88,7 @@
        (lsp +eglot)              ; M-x vscode
        (magit +forge)               ; a git porcelain for Emacs
        make                ; run make tasks from Emacs
-       ;;pass              ; password manager for nerds
+       pass              ; password manager for nerds
        pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
        ;;rgb               ; creating color strings
