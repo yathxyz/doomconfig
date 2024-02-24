a4c3e350d1ba2f22f51cdfb7bbfe15267d66018d
Ioannis Eleftheriou <me@yath.xyz>
2024-02-24 06:52:48 +0000

    Make minimal

--- config.el
+++ config.el
@@ -1,10 +1,10 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
 (setq user-full-name "Ioannis Eleftheriou"
-      user-mail-address "me@yath.xyz"
-      epg-user-id user-mail-address)
+      user-mail-address "me@yath.xyz")
+;;      epg-user-id user-mail-address)
 
-(setq doom-font (font-spec :family "Hack Nerd Font Mono" :size 16))
+;;(setq doom-font (font-spec :family "Hack Nerd Font Mono" :size 16))
 ;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
 
 (setq doom-theme 'doom-oksolar-dark)
@@ -13,34 +13,33 @@
 
 (setq org-directory (getenv "WORKDIR"))
 
-
-(setq! citar-bibliography (list (concat (getenv "WORKDIR") "librarium/library.bib")))
+;;(setq! citar-bibliography (list (concat (getenv "WORKDIR") "librarium/library.bib")))
 
 (map! :leader :desc "Insert org mode timestamp at point with current date and time"
       :n "y d t" #'insert-now-timestamp-inactive)
 (setq! org-roam-directory (concat org-directory "roam/"))
 
-(defun refresh-citar-bibliography ()
-  (interactive)
-  (setq! citar-bibliography
-         (directory-files-recursively (concat (getenv "WORKDIR") "librarium/") "\\.bib$"))
-  (message "Refreshed list of bib files!"))
+;;(defun refresh-citar-bibliography ()
+;;  (interactive)
+;;  (setq! citar-bibliography
+;;         (directory-files-recursively (concat (getenv "WORKDIR") "librarium/") "\\.bib$"))
+;;  (message "Refreshed list of bib files!"))
 
-(setq! citar-citeproc-csl-styles-dir "~/Zotero/styles/")
+;;(setq! citar-citeproc-csl-styles-dir "~/Zotero/styles/")
 
-(setq! citar-library-paths
-       (list (concat (getenv "WORKDIR") "librarium/")))
+;;(setq! citar-library-paths
+;;       (list (concat (getenv "WORKDIR") "librarium/")))
 
-(setq! citar-notes-paths (list (concat org-roam-directory "references/")))
+;;(setq! citar-notes-paths (list (concat org-roam-directory "references/")))
 
 (map! :leader :desc "Toggle centered window mode" :n "y c" #'centered-window-mode)
 (map! :leader :desc "Toggle automatic fill mode" :n "y a" #'auto-fill-mode)
-(map! :leader :desc "Find bibliographic entry" :n "y b e" #'citar-open-entry)
-(map! :leader :desc "Open document from bib entry" :n "y o" #'citar-open)
+;;(map! :leader :desc "Find bibliographic entry" :n "y b e" #'citar-open-entry)
+;;(map! :leader :desc "Open document from bib entry" :n "y o" #'citar-open)
 (map! :leader :desc "Fill region" :n "y w" #'fill-region)
 ;;(map! :leader :desc "Toggle global flycheck mode" :n "y f f f" #'global-flycheck-mode)
-(map! :leader :desc "Refresh bib files" :n "y b r" #'refresh-citar-bibliography)
-(map! :leader :desc "Open URL in bibtex entry" :n "y l" #'citar-open-links)
+;;(map! :leader :desc "Refresh bib files" :n "y b r" #'refresh-citar-bibliography)
+;;(map! :leader :desc "Open URL in bibtex entry" :n "y l" #'citar-open-links)
 (map! :leader :desc "Toggle autocompletion" :n "y x" #'+company/toggle-auto-completion)
 (map! :leader :desc "Open elfeed" :n "y n" #'elfeed)
 ;;(map! :leader :desc "Quick ai prompt in the buffer" :n "y q" #'org-ai-prompt)
@@ -50,7 +49,7 @@
       org-journal-file-format "%Y%m%d.org"
       org-journal-date-prefix "#+TITLE: ")
 
-(setq deft-directory (concat (getenv "WORKDIR") "roam/"))
+;;(setq deft-directory (concat (getenv "WORKDIR") "roam/"))
 (setq company-idle-delay 0)
 
 (use-package! websocket
@@ -71,8 +70,8 @@
 
 ;; The silliest hack I could think of. Automatically filter only for news
 ;; by adding it to the tail of the elfeed hook.
-(defun elfeed-set-default-filter ()
-  (setq elfeed-search-filter "@2-weeks-ago +news"))
-
-(add-hook 'elfeed-search-mode-hook #'elfeed-set-default-filter)
-(add-hook 'elfeed-search-mode-hook #'elfeed-update)
+;;(defun elfeed-set-default-filter ()
+;;  (setq elfeed-search-filter "@2-weeks-ago +news"))
+;;
+;;(add-hook 'elfeed-search-mode-hook #'elfeed-set-default-filter)
+;;(add-hook 'elfeed-search-mode-hook #'elfeed-update)
--- init.el
+++ init.el
@@ -1,6 +1,5 @@
 ;;; init.el -*- lexical-binding: t; -*-
 
-
 (doom! :input
        ;;bidi              ; (tfel ot) thgir etirw uoy gnipleh
        ;;chinese
@@ -8,19 +7,18 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       (company +childframe)           ; the ultimate code completion backend
+       company           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
-       (vertico +childframe
-                +icons)          ; the search engine of the future
+       vertico           ; the search engine of the future
 
        :ui
-       deft              ; notational velocity for Emacs
+       ;;deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
        ;;doom-quit         ; DOOM quit-message prompts when you quit Emacs
-       (emoji +unicode)  ; 🙂
+       ;;(emoji +unicode)  ; 🙂
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
        ;;indent-guides     ; highlighted indent columns
@@ -32,33 +30,32 @@
        ophints           ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        ;;tabs              ; a tab bar for Emacs
-       treemacs          ; a project drawer, like neotree but cooler
-       unicode           ; extended unicode support for various languages
+       ;;treemacs          ; a project drawer, like neotree but cooler
+       ;;unicode           ; extended unicode support for various languages
        (vc-gutter +pretty) ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        ;;window-select     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
-       zen               ; distraction-free coding or writing
+       ;;zen               ; distraction-free coding or writing
 
        :editor
        (evil +everywhere); come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
-       (format +onsave)  ; automated prettiness
+       ;;(format +onsave)  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
-       multiple-cursors  ; editing in many places at once
+       ;;multiple-cursors  ; editing in many places at once
        ;;objed             ; text object editing for the innocent
        ;;parinfer          ; turn lisp into python, sort of
        ;;rotate-text       ; cycle region at point between text candidates
        snippets          ; my elves. They type so I don't have to
-       word-wrap         ; soft wrapping with language-aware indent
+       ;;word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
-       (dired +icons
-              +ranger)             ; making dired pretty [functional]
+       dired             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
-       (ibuffer +icons)         ; interactive buffer management
+       ;;ibuffer         ; interactive buffer management
        undo              ; persistent, smarter undo for your inevitable mistakes
        vc                ; version-control and Emacs, sitting in a tree
 
@@ -66,54 +63,50 @@
        ;;eshell            ; the elisp shell that works everywhere
        ;;shell             ; simple shell REPL for Emacs
        ;;term              ; basic terminal emulator for Emacs
-       vterm             ; the best terminal emulation in Emacs
+       ;;vterm             ; the best terminal emulation in Emacs
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
-       (spell +flyspell) ; tasing you for misspelling mispelling
-       grammar           ; tasing grammar mistake every you make
+       ;;(spell +flyspell) ; tasing you for misspelling mispelling
+       ;;grammar           ; tasing grammar mistake every you make
 
        :tools
        ;;ansible
-       biblio            ; Writes a PhD for you (citation needed)
+       ;;biblio            ; Writes a PhD for you (citation needed)
        ;;collab            ; buffers with friends
-       (debugger +lsp)         ; FIXME stepping through code, to help you add bugs
-       direnv
-       docker
-       editorconfig      ; let someone else argue about tabs vs spaces
+       ;;debugger          ; FIXME stepping through code, to help you add bugs
+       ;;direnv
+       ;;docker
+       ;;editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
-       ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
-       (lsp +eglot)              ; M-x vscode
-       (magit +forge)               ; a git porcelain for Emacs
-       make                ; run make tasks from Emacs
-       pass              ; password manager for nerds
-       pdf               ; pdf enhancements
+       ;;lsp               ; M-x vscode
+       magit             ; a git porcelain for Emacs
+       ;;make              ; run make tasks from Emacs
+       ;;pass              ; password manager for nerds
+       ;;pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
        ;;rgb               ; creating color strings
        ;;taskrunner        ; taskrunner for all your projects
        ;;terraform         ; infrastructure as code
-       tmux              ; an API for interacting with tmux
-       tree-sitter       ; syntax and parsing, sitting in a tree...
-       upload            ; map local to remote projects via ssh/ftp
+       ;;tmux              ; an API for interacting with tmux
+       ;;tree-sitter       ; syntax and parsing, sitting in a tree...
+       ;;upload            ; map local to remote projects via ssh/ftp
 
        :os
-       (:if IS-MAC macos)  ; improve compatibility with macOS
-       tty               ; improve the terminal Emacs experience
+       (:if (featurep :system 'macos) macos)  ; improve compatibility with macOS
+       ;;tty               ; improve the terminal Emacs experience
 
        :lang
        ;;agda              ; types of types of types of types...
        ;;beancount         ; mind the GAAP
-       (cc +lsp
-           +tree-sitter)         ; C > C++ == 1
+       ;;(cc +lsp)         ; C > C++ == 1
        ;;clojure           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
-       (csharp +dotnet
-               +lsp
-               +tree-sitter)            ; unity, .NET, and mono shenanigans
+       ;;csharp            ; unity, .NET, and mono shenanigans
        ;;data              ; config/data formats
        ;;(dart +flutter)   ; paint ui and not much else
        ;;dhall
@@ -138,25 +131,20 @@
        ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
-       (latex +cdlatex
-              +fold
-              +latexmk
-              +lsp)             ; writing papers in Emacs has never been so fun
+       ;;latex             ; writing papers in Emacs has never been so fun
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
        ;;lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
-       (nix +tree-sitter)               ; I hereby declare "nix geht mehr!"
+       ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        (org +roam2
-            +journal
-            +pomodoro)               ; organize your plain life in plain text
+            +journal)              ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       (python +lsp
-               +tree-sitter)           ; beautiful is better than ugly
+       ;;python            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
@@ -172,21 +160,20 @@
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        ;;web               ; the tubes
-       (yaml +lsp
-             +tree-sitter)               ; JSON, but readable
+       ;;yaml              ; JSON, but readable
        ;;zig               ; C, but simpler
 
        :email
-       (mu4e +org +gmail)
+       ;;(mu4e +org +gmail)
        ;;notmuch
        ;;(wanderlust +gmail)
 
        :app
-       calendar
+       ;;calendar
        ;;emms
        ;;everywhere        ; *leave* Emacs!? You must be joking
        ;;irc               ; how neckbeards socialize
-       (rss +org)        ; emacs as an RSS reader
+       ;;(rss +org)        ; emacs as an RSS reader
        ;;twitter           ; twitter client https://twitter.com/vnought
 
        :config
